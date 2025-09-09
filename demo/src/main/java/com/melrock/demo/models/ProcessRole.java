80a1250dfa8c56d91fc8deb8bcc4e3629e8fe6e3
AranEspejo <133788790+AranEspejo@users.noreply.github.com>
2025-09-08 23:32:22 -0500

    Add files via upload

---
+++ demo/src/main/java/com/melrock/demo/models/ProcessRole.java
@@ -0,0 +1,29 @@
+package com.melrock.demo.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProcessRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "company_id", nullable = false)
+    private Company company;
+}
