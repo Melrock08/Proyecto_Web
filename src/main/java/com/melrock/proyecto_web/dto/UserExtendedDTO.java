b4fab018b4cf57173bc8f667dcc41cdf6d6e0f64
Juan Bernardo Uribe <uribejuanb@javeriana.edu.co>
2025-11-23 15:24:06 -0500

    JwtUtils con excepciones y DTOs necesarios correctamente integrados

---
+++ src/main/java/com/melrock/proyecto_web/dto/UserExtendedDTO.java
@@ -0,0 +1,17 @@
+package com.melrock.proyecto_web.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserExtendedDTO extends UsuarioDTO {
+    EmpresaDTO empresa;
+}
